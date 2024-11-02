4b49e101c879b0cc6ffcc4184f6b9101d1879e8a
zheer barzan <zheerbarzan85@outlook.com>
2024-11-02 20:01:48 +0300

    fuck Xcode

---
+++ tasked/Views/ListView.swift
@@ -0,0 +1,25 @@
+//
+//  ListView.swift
+//  tasked
+//
+//  Created by Zheer Barzan on 2/11/24.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    var body: some View {
+        List{
+            HStack{
+                
+            }
+        }.navigationTitle("To Do List ✅")
+    }
+}
+
+#Preview {
+    NavigationView{
+    
+        ListView()
+    }
+}
--- tasked/taskedApp.swift
+++ tasked/taskedApp.swift
@@ -11,7 +11,10 @@ import SwiftUI
 struct taskedApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView{
+                ListView()
+            }
+            
         }
     }
 }
