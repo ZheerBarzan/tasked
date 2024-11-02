98335d131d965dc96cd0c2c9d5a1bb9ce67cb93c
zheer barzan <zheerbarzan85@outlook.com>
2024-11-02 19:40:13 +0300

    Initial Commit

---
+++ tasked/taskedApp.swift
@@ -0,0 +1,17 @@
+//
+//  taskedApp.swift
+//  tasked
+//
+//  Created by Zheer Barzan on 2/11/24.
+//
+
+import SwiftUI
+
+@main
+struct taskedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
